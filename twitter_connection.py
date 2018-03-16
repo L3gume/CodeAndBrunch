40b67eae7d43ac8fc60babfb1a151d87de1c463e
evanlaflamme <evanlaflamme@hotmail.com>
2018-03-16 13:02:36 -0400

    Get tweets with patrick in name

---
+++ twitter_connection.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+import twitter
+api = twitter.Api(consumer_key='ghEPZheOTZDGLveQicPtGV39i',
+                  consumer_secret='2lTOCuSv4Y1e7Oad7ROP4F8pWTYXaabhIdgBmASh5ygRHA7Dp7',
+                  access_token_key='974684154750230528-uHKLAffr0ouHqL7j9WCBT0MgEUFIeUN',
+                  access_token_secret='c21kbxb9jRGImDwjWXyOqDdH36OzgfS2eZWy8BcgtnDsH')
+
+tweets = api.GetSearch(term="patrick")
+
+for tweet in tweets:
+    print(tweet)
