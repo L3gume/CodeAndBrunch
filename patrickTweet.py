5fc427107706142aa8a76f0a833772843a03f97a
Michael Vaquier <michael.vaquier@mail.mcgill.ca>
2018-03-16 13:22:13 -0400

    Hey I'm trying ok

--- patrickTweet.py
+++ patrickTweet.py
@@ -3,10 +3,13 @@
 # PatrickTweet
 # A very useful command-line tool that gives you a random tweet with "Patrick" in it.
 
-from CodeAndBrunch import Message
-from CodeAndBrunch import JSONParser
-
 import twitter
+import random
+import json
+
+from json import Message
+from json import JSONParser
+
 api = twitter.Api(consumer_key='ghEPZheOTZDGLveQicPtGV39i',
                   consumer_secret='2lTOCuSv4Y1e7Oad7ROP4F8pWTYXaabhIdgBmASh5ygRHA7Dp7',
                   access_token_key='974684154750230528-uHKLAffr0ouHqL7j9WCBT0MgEUFIeUN',
@@ -14,8 +17,7 @@ api = twitter.Api(consumer_key='ghEPZheOTZDGLveQicPtGV39i',
 
 tweets = api.GetSearch(term="patrick")
 
-print("yo your script works")
-
-def foo():
-    return 1
-
+def randomTweet(tweet, jsonParser):
+    jsonTweet = tweet[random.randint(0, len(tweet) - 1)]
+    message = jsonParser.parseJSON(jsonTweet)
+    message.prettyPrint()
