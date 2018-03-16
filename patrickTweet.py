684f9176ad90a324b07a16cb6ec7707c846f553e
L3gume <justin.tremblay@mail.mcgill.ca>
2018-03-16 13:09:14 -0400

    Added imports

--- patrickTweet.py
+++ patrickTweet.py
@@ -3,6 +3,9 @@
 # PatrickTweet
 # A very useful command-line tool that gives you a random tweet with "Patrick" in it.
 
+from CodeAndBrunch import Message
+from CodeAndBrunch import JSONParser
+
 print("yo your script works")
 
 def foo():
